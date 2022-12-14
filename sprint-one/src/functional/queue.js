7fd4def84bf69719c84f39a829e9e81dbc2427d3
qchen <quanjingchen@gmail.com>
2022-12-14 12:27:31 -0800

    complete 1.functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,35 @@
 var Queue = function() {
-  var someInstance = {};
+
+  var someInstance = {
+    data: storage,
+    head: 0,
+    tail: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[someInstance.tail] = value;
+    someInstance.tail++;
   };
 
   someInstance.dequeue = function() {
+    var len = Object.keys(storage).length;
+    if (len === 0) {
+      return undefined;
+    } else {
+      var element = storage[someInstance.head];
+      delete storage[someInstance.head];
+      someInstance.head++;
+      return element;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+
   };
 
   return someInstance;
