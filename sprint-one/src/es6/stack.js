d3845478c9d1497da381a5c5416a3d37419f7eba
qchen <quanjingchen@gmail.com>
2022-12-14 15:56:52 -0800

    Complete 5) es6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var len = Object.keys(this.storage).length;
+    this.storage[len] = value;
+  }
+
+  pop() {
+    var len = Object.keys(this.storage).length;
+    if (len === 0) {
+      return undefined;
+    } else {
+      var element = this.storage[len - 1];
+      delete this.storage[len - 1];
+      return element;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
