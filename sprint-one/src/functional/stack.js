7fa6d5fbcd55a70e189faca1da67aaf73d525c2d
qchen <quanjingchen@gmail.com>
2022-12-14 13:44:51 -0800

    Complete 1)functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
+
+  var someInstance = {
+    data: storage
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var len = Object.keys(storage).length;
+    storage[len] = value;
   };
 
   someInstance.pop = function() {
+    var len = Object.keys(storage).length;
+    if (len === 0) {
+      return undefined;
+    } else {
+      var element = storage[len - 1];
+      delete storage[len - 1];
+      return element;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
