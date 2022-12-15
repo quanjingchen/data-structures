8dc49987fdf412033dd09597856fbdf8f47fff9d
qchen <quanjingchen@gmail.com>
2022-12-15 09:28:39 -0800

    Complete 1)Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,23 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (this.head === null) {
+      var newNode = Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var output = this.head.value;
+    this.head = this.head.next;
+    return output;
   };
 
   list.contains = function(target) {
+    var memo = this.head;
+    while (memo != null) {
+      if (memo.value === target) {
+        return true;
+      }
+      memo = memo.next;
+    }
+    return false;
   };
-
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
